ae754026f7c0763f5b63408472b3f219e9a87e55
kazmiali123 <kazmiali123@gmail.com>
2024-02-13 02:51:01 -0500

    changed chunkSizeWarningLimit in vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 900,
+  },
   plugins: [react()],
   server: {
     port: 3000,
