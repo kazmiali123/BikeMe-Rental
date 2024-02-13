71f55a72ad2c38d59dda3031613728cba51a2b9f
kazmiali123 <kazmiali123@gmail.com>
2024-02-13 04:23:03 -0500

    package.json testing7

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://<kazmialiMongoDB>:<KazmiAli123Mongo>@cluster0.7zbv0zf.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://kazmialiMongoDB:KazmiAli123Mongo@cluster0.7zbv0zf.mongodb.net/?retryWrites=true&w=majority');
 
 module.exports = mongoose.connection;
 
